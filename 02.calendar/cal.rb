d05a61213c0a9b936335261b8c889d0961c889c5
mikan-v7y <mikan.v7y@gmail.com>
2024-10-28 22:37:06 +0900

    指摘事項を修正しました

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -6,74 +6,52 @@ require 'date'
 # optparserクラスを使うために宣言
 require 'optparse'
 
-# -mオプションを使って月を、-yオプションを使って年を表示する
-
-# コマンドライン引数のオプションを定義
-hash = {}
+options = {}
 opt = OptionParser.new
 
-# -mオプションを定義
 opt.on('-m MONTH'){|m|
-    hash[:month] = m.to_i
+    options[:month] = m.to_i
 }
 
-#  -yオプションを定義
 opt.on('-y YEAR'){|y|
-    hash[:year] = y.to_i
+    options[:year] = y.to_i
 }
 
-# -mと-yの後ろで渡した引数を解析する。これにより、定義したオプションが作動する。
 opt.parse!(ARGV)
 
-# -mに引数が指定されていない場合は、今月を表示する
-if hash[:month] == nil
-    hash[:month] = Date.today.month
+if options[:month] == nil
+    options[:month] = Date.today.month
 end
 
-# -yに引数が指定されていない場合は、今年を表示する
-if hash[:year] == nil
-    hash[:year] = Date.today.year
+if options[:year] == nil
+    options[:year] = Date.today.year
 end
 
-# 月は1~12の範囲で入力できる
-if hash[:month] < 1 || hash[:month] > 12
+if options[:month] < 1 || options[:month] > 12
     puts "Month can be entered from 1 to 12"
+    abort
 end
 
-# 年は1970年~2100年の範囲で入力できる
-if hash[:year] < 1970 || hash[:year] > 2100
+if options[:year] < 1970 || options[:year] > 2100
     puts "Year can be entered from 1970 to 2100"
+    abort
 end
 
-# カレンダーの表示画面を作る
+CALENDAR_WIDTH = 20
+puts "#{options[:month]}月 #{options[:year]}".center(CALENDAR_WIDTH)
 
-# カレンダーに年月を中央寄せで表示する
-calender_width = 20
-puts "#{hash[:month]}月 #{hash[:year]}".center(calender_width)
+WEEKDAYS = ["日", "月", "火", "水", "木", "金", "土"]
+puts WEEKDAYS.join(" ")
 
-# カレンダーに曜日を表す
-Sun_to_Sat = ["日", "月", "火", "水", "木", "金", "土"]
-puts Sun_to_Sat.join(" ")
+first_day = Date.new(options[:year], options[:month], 1)
+last_day = Date.new(options[:year], options[:month], -1)
 
-# カレンダーの日にち（1日~31日）を取得する
-first_day = Date.new(hash[:year], hash[:month], 1)
-last_day = Date.new(hash[:year], hash[:month], -1)
-
-days = Enumerator.new { |yielder|
-    (first_day..last_day).each do |date|
-        yielder << date
-    end
-}
-
-# 最初の曜日に対するスペース
 print " " * (first_day.wday * 3)
 
-# カレンダーの日にちを出力。土曜日なら改行
-days.each do |date|
+(first_day..last_day).each do |date|
     print date.day.to_s.rjust(2) + " "
 
-    puts if date.wday == 6
+    puts if date.saturday?
 end
 
-# カレンダー終了後の改行
 puts
