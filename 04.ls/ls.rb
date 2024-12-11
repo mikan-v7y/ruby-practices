57a1defb9c36c999aa554b21ce0375d1f40bbdb7
mikan-v7y <mikan.v7y@gmail.com>
2024-12-11 15:05:14 +0900

    refactor: 可読性向上のため、変数名とメソッド内を修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -10,18 +10,16 @@ def main
 end
 
 def files_display_arrangement(files)
-  longest_file_characters = files.map(&:size).max
-  files_after_arrangement = files.map { |file| file.ljust(longest_file_characters + COLUMN_GAP) }
-
-  files_number = files.size
-  quotient, remainder = files_number.divmod(COLUMN)
-  rows_number = remainder.zero? ? quotient : quotient + 1
-
-  ls_format_files = files_dimensional_array(files_after_arrangement, rows_number)
+  max_file = files.map(&:size).max
+  files_after_arrangement = files.map { |file| file.ljust(max_file + COLUMN_GAP) }
+  ls_format_files = files_dimensional_array(files_after_arrangement)
   show_file(ls_format_files)
 end
 
-def files_dimensional_array(files, rows_number)
+def files_dimensional_array(files)
+  quotient, remainder = files.size.divmod(COLUMN)
+  rows_number = remainder.zero? ? quotient : quotient + 1
+
   ls_format_files = Array.new(rows_number) { Array.new(COLUMN) }
   files.each_with_index do |file, i|
     column, row = i.divmod(rows_number)
