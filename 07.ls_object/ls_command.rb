8f9fd0baa898e13c71f2f8c17e7066b0a2f9582a
mikan-v7y <mikan.v7y@gmail.com>
2025-09-19 03:10:01 +0900

    feat: コマンドライン引数の処理に関する基本的なクラス構造を作成

--- 07.ls_object/ls_command.rb
+++ 07.ls_object/ls_command.rb
@@ -1,1 +1,7 @@
 # frozen_string_literal: true
+
+class LsCommand
+  def initialize(argv)
+    @options = LsOption.new(argv)
+  end
+end
--- 07.ls_object/ls_option.rb
+++ 07.ls_object/ls_option.rb
@@ -1,1 +1,33 @@
 # frozen_string_literal: true
+
+class LsOption
+  def initialize(argv)
+    @all = false
+    @long = false
+    @reverse = false
+
+    parse(argv)
+  end
+
+  private
+
+  def parse(argv)
+    opt = OptionParser.new
+    options = {}
+
+    opt.on('-a') do
+      @all = true
+    end
+
+    opt.on('-l') do
+      @long = true
+    end
+
+    opt.on('-r') do
+      @reverse = true
+    end
+
+    opt.parse!(argv)
+    options
+  end
+end
