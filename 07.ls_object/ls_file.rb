7b67c9da8c02f5eee3557a5e25b940acdbcd44a8
mikan-v7y <mikan.v7y@gmail.com>
2025-09-19 03:24:45 +0900

    feat: ファイルタイプとファイルモードを取得するメソッドを追加

--- 07.ls_object/ls_file.rb
+++ 07.ls_object/ls_file.rb
@@ -36,4 +36,24 @@ class LsFile
       @name
     ].join(' ')
   end
+
+  private
+
+  def file_type_and_permissions
+    file_type = FILE_TYPE_LIST.fetch(@stat.ftype)
+
+    mode_number = @stat.mode.to_s(8)
+
+    owner_permission = mode_number[-3]
+    group_permission = mode_number[-2]
+    other_permission = mode_number[-1]
+
+    permissions = [
+      FILEMODE_PERMISSION_LIST.fetch(owner_permission),
+      FILEMODE_PERMISSION_LIST.fetch(group_permission),
+      FILEMODE_PERMISSION_LIST.fetch(other_permission)
+    ].join
+
+    file_type + permissions
+  end
 end
