bc8976c99387ca0764f735022eba316d111cab64
mikan-v7y <mikan.v7y@gmail.com>
2025-09-19 03:22:26 +0900

    feat: ファイルの詳細情報を取得するメソッドを作成

--- 07.ls_object/ls_file.rb
+++ 07.ls_object/ls_file.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
+require 'etc'
+
 class LsFile
   def initialize(name)
     @name = name
     @stat = File.stat(@name)
+  end
+
+  def details
+    [
+      file_type_and_permissions,
+      @stat.nlink.to_s.rjust(2),
+      Etc.getpwuid(@stat.uid).name,
+      Etc.getgrgid(@stat.gid).name,
+      @stat.size.to_s.rjust(5),
+      @stat.mtime.strftime('%b %d %H:%M'),
+      @name
+    ].join(' ')
   end
 end
