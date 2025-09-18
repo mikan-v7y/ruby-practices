120417f6bda51950d15452a6c77d219309c6d507
mikan-v7y <mikan.v7y@gmail.com>
2025-09-19 03:11:34 +0900

    feat: lsコマンドの基本的なロジック部分を作成

--- 07.ls_object/ls_command.rb
+++ 07.ls_object/ls_command.rb
@@ -4,4 +4,21 @@ class LsCommand
   def initialize(argv)
     @options = LsOption.new(argv)
   end
+
+  def run
+    files = Dir.entries('.')
+    files.reject! { |f| f.start_with?('.') } unless @options.all?
+    files.sort!
+    files.reverse! if @options.reverse?
+
+    if @options.long?
+      display_total_blocks(files)
+
+      files.each do |file|
+        puts LsFile.new(file).details
+      end
+    else
+      display_files_in_ls_format(files)
+    end
+  end
 end
