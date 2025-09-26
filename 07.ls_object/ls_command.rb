05010bf44c4577d0d0b4bfeb5f8940c1754ed126
mikan-v7y <mikan.v7y@gmail.com>
2025-09-27 01:44:05 +0900

    refactor: 凝集度を高めるため、メソッドを細分化

--- 07.ls_object/ls_command.rb
+++ 07.ls_object/ls_command.rb
@@ -39,19 +39,34 @@ class LsCommand
   def display_files_in_ls_format(files)
     return if files.empty?
 
+    column_width = calculate_column_width(files)
+    columns_number = calculate_columns_number(column_width)
+    rows = build_rows(files, column_width, columns_number)
+
+    print_rows(rows)
+  end
+
+  def calculate_column_width(files)
     max_file_characters = files.map(&:size).max
-    column_width = max_file_characters + COLUMN_PADDING
+    max_file_characters + COLUMN_PADDING
+  end
 
+  def calculate_columns_number(column_width)
     _, terminal_width = IO.console.winsize
-    columns_number = [terminal_width / column_width, MINIMUM_COLUMN_NUMBER].max
-    rows_number = (files.size.to_f / columns_number).ceil
+    [terminal_width / column_width, MINIMUM_COLUMN_NUMBER].max
+  end
 
+  def build_rows(files, column_width, columns_number)
+    rows_number = (files.size.to_f / columns_number).ceil
     rows = Array.new(rows_number) { [] }
     files.each_with_index do |file, i|
-      row_index_to_be_placed = i % rows_number
-      rows[row_index_to_be_placed] << file.ljust(column_width)
+      row_index = i % rows_number
+      rows[row_index] << file.ljust(column_width)
     end
+    rows
+  end
 
+  def print_rows(rows)
     rows.each { |row| puts row.join }
   end
 end
