46646361df17fa18dc9b2cbb38ef0f99125c12e1
mikan-v7y <mikan.v7y@gmail.com>
2025-09-29 15:54:39 +0900

    feat: 列数の計算を行うクラスを作成

---
+++ 07.ls_object/column_calculator.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class ColumnCalculator
+  MINIMUM_COLUMN_NUMBER = 1
+
+  def initialize(files, column_padding: 3)
+    @files = files
+    @column_padding = column_padding
+  end
+
+  def column_width
+    max_file_characters = @files.map(&:size).max
+    max_file_characters + @column_padding
+  end
+
+  def calculate
+    _, terminal_width = IO.console.winsize
+    [terminal_width / column_width, MINIMUM_COLUMN_NUMBER].max
+  end
+end
